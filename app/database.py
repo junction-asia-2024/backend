7397e9787657c3cd56f1811e6059d6dbaa9ca421
yu-heejin <jkgt0521@naver.com>
2024-08-08 21:42:33 +0900

    feat: 테스트용 MySQL 연결 및 docker-compose 파일 추가

--- app/database.py
+++ app/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# SQLALCHEMY_DATABASE_URL = "mysql+mysqldb://root:junction123!@db:3306/junction"
-SQLALCHEMY_DATABASE_URL = "mariadb+pymysql://root:d4594283!@localhost:3306/junction"
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:junction123!@db:3306/junction"
+# SQLALCHEMY_DATABASE_URL = "mariadb+pymysql://root:d4594283!@localhost:3306/junction"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
